41bef4d6cb03d027c16d16d64c4b8fbd71f5ed86
Axelw578 <axelbix578@gmail.com>
2024-10-07 21:26:15 -0600

    Agregar archivos de proyecto.

---
+++ Excembly_vAlpha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Excembly_vAlpha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
